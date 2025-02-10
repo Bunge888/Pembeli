e71bf0cec5aab9afb4a979149ed42a8c5a26c155
Bunge888 <burhandoang59@gmail.com>
2025-02-10 09:35:12 +0700

    menambah fungsi Ambil daftar ke keranjang

--- main.js
+++ main.js
@@ -23,8 +23,12 @@ const firebaseConfig = {
   appId: "1:383282706725:web:bf9fd56cea37cb3a0fc2cc",
   measurementId: "G-31EQF6PDVN"
 };
-// fungsi daftar barang
 
+// inisialisasi firebase
+const aplikasi = initializeApp(firebaseConfig)
+const basisdata = getFirestore(aplikasi)
+
+ // fungsi ambil daftar barang
 export async function ambilDaftarInventory() {
   const refDokumen = collection(basisdata, "inventory");
   const kueri = query(refDokumen, orderBy("item"));
@@ -44,9 +48,51 @@ export async function ambilDaftarInventory() {
   return hasilKueri;
 }
 
-//inisialisasi firebase
-const aplikasi = initializeApp(firebaseConfig)
-const basisdata = getFirestore(aplikasi)
+// menambah barang ke keranjang
+export async function tambahbarangkekeranjang(
+  idbarang,
+  harga,
+  jumlah,
+  idpelanggan,
+  namapelanggan
+  ) {
+    
+
+ try {
+    // menyimpan data ke collection transaksi
+    const refDokumen = await addDoc(collection(basisdata, "transaksi"), {
+      idbarang: idbarang,
+      nama: nama,
+      harga: harga,
+      jumlah: jumlah,
+      idpelanggan: idpelanggan,
+      namapelanggan: namapelanggan
+    })
+    
+    // menampilkan pesan berhasil
+    console.log("berhasil menyimpan keranjang")
+  } catch (error) {
+    // menampilkan pesan gagal
+    console.log(error)    
+  }
+} 
 
 
+// menampilkan barang di keranjang
+export async function ambilDaftarBarangDiKeranjang() {
+  const refDokumen = collection(basisdata, "transaksi");
+  const kueri = query(refDokumen, orderBy("nama"));
+  const cuplikanKueri = await getDocs(kueri);
 
+  let hasilKueri = [];
+  cuplikanKueri.forEach((dokumen) => {
+    hasilKueri.push({
+      id: dokumen.id,
+      nama: dokumen.data().nama,
+      jumlah: dokumen.data().jumlah,
+      harga: dokumen.data().harga
+    })
+  })
+
+  return hasilKueri;
+}
